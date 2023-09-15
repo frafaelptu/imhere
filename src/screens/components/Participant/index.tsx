70e7f5bf501431dd3fe902db456142b237285687
Francisco Rafael <francisco.gomes@viavarejo.com.br>
2023-09-15 06:18:15 -0300

    imhere project react native

---
+++ src/screens/components/Participant/index.tsx
@@ -0,0 +1,29 @@
+import { GestureResponderEvent, Text, TouchableOpacity, View } from "react-native";
+
+import { styles } from "./styles";
+import React from "react";
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+}
+
+
+export function Participant({ name, onRemove }: Props) {
+
+   return (
+    <View style={styles.container}>
+      <Text style={styles.name}>
+        {name}
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={onRemove}>
+        <Text style={styles.buttonText}>
+          -
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+
+
